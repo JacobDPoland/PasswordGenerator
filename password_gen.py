bf963a41725fdfccaa42e17b1af78d3d3901280d
JacobDPoland <45803275+JacobDPoland@users.noreply.github.com>
2022-11-24 16:10:33 -0600

    Initial release

---
+++ password_gen.py
@@ -0,0 +1,18 @@
+# passwords can use ascii characters #33 through #126
+# but here I will be using the string library
+
+import random
+import string
+
+# used this to find valid indices are 0 to 93
+# for i, c in enumerate(string.printable):
+#     print(i, c)
+
+password_length = 16
+password = ""
+for i in range(password_length):
+    random_value = random.random()  # decimal value between 0 and 1
+    index = int(random_value * 93)
+    password += string.printable[index]
+
+print(password)
